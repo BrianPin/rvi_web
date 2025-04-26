aa249d44575ec60be3c17eef148d75d0b0cf3f92
Brian Pin <shaohuang@gmail.com>
2025-04-26 13:42:26 -0700

    First render of hello template

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,19 @@
+from datetime import datetime
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello Django")
+
+def hello_there(request, name='RVI'):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
